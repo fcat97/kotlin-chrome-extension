fcda20eede96c2411ac803a764510bb4ad0c7261
Shahriar Zaman <nayeem.zxc@gmail.com>
2024-07-10 10:50:24 +0600

    - popup height set Popup/index.html should have a fixed height. Compose can't update the popup's window height at runtime.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,23 @@ plugins {
 
 tasks.register("preparePopup") {
     group = "extension"
+    dependsOn(":extensionComponent:popup:clean")
     dependsOn(":extensionComponent:popup:wasmJsBrowserDistribution")
+        .mustRunAfter(":extensionComponent:popup:clean")
 
     doLast {
         copy {
             from("extensionComponent/popup/build/dist/wasmJs/productionExecutable")
+            into("distribution/popup/")
+        }
+
+        /** __IMPORTANT NOTE__
+         * This is required for extension/popup to set body height manually to fixed pixel.
+         * The height of popup can't be updated runtime.
+         * Setting Modifier.height() to root composable doesn't work.
+         */
+        copy {
+            from("extensionComponent/popup/src/wasmJsMain/resources/styles.css")
             into("distribution/popup/")
         }
     }
@@ -20,7 +32,9 @@ tasks.register("preparePopup") {
 
 tasks.register("prepareOptions") {
     group = "extension"
+    dependsOn(":extensionComponent:options:clean")
     dependsOn(":extensionComponent:options:wasmJsBrowserDistribution")
+        .mustRunAfter(":extensionComponent:options:clean")
 
     doLast {
         copy {
